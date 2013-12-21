30c87223e60fc6e070017a7c37e62997e201929b
Teddy <i.kenting@gmail.com>
2013-12-21 23:03:30 +0800

    move functional_tests to functional_tests app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     """"""
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
         
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
@@ -32,14 +32,14 @@ class NewVisitorTest(unittest.TestCase):
         # She is invited to enter a to-do item straight way
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertEqual(
-                 inputbox.get_attribute('placeholder'),
-                 'Enter a to-do item'
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
         )
-        
+
         # She type "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
-        
+
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
         inputbox.send_keys(Keys.ENTER)
@@ -73,8 +73,3 @@ class NewVisitorTest(unittest.TestCase):
         
         # Satisfied, she goes back to sleep
         self.fail('Finish the test!')
-        
-        
-        
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
